075b37c6f5e2b5a273109fa9dead619f4fbd2c8c
Miguel Cabral Parece <ist1103369@tecnico.ulisboa.pt>
2025-03-28 12:43:27 +0000

    restarts

--- Exceptional.jl
+++ Exceptional.jl
@@ -1,3 +1,4 @@
+import Base.identity
 struct SignalException <: Exception 
     exception::Exception
     must_handle::Bool
@@ -5,6 +6,11 @@ struct SignalException <: Exception
 end
 
 struct EndOfLine <: Exception end
+struct ExitException <: Exception 
+    
+    token::Any
+    value::Any
+end
 
 struct DivisionByZero <: Exception end
 
@@ -22,8 +28,10 @@ function error(exception)
     
     for (name, handler) in reverse(handlers)
         
-        if name == exception # encontrou um handler para a excepcao
-            handler(exception)
+        if exception == name # encontrou um handler para a excepcao
+            println("Handler found, calling handler")
+            
+            return handler(exception)
             handled = true
             break
         end
@@ -40,7 +48,8 @@ function signal(exception)
     #se existirem, chamar o handler
     handlers = get(task_local_storage(), HANDLERS_KEY, Pair{Type{<:Exception}, Function}[])
     handled = false
-   
+
+
     for (name, handler) in reverse(handlers)
         
         if name == exception # encontrou um handler para a excepcao
@@ -65,6 +74,7 @@ function handling(f, handlers) # funcao F e pairs de handlers
     orignal_size = length(current_handlers)
 
     for (exception, handler) in handlers
+        println("Adding handler for exception: ", exception)
         push!(current_handlers, (exception => handler))
     end
     try
@@ -73,7 +83,6 @@ function handling(f, handlers) # funcao F e pairs de handlers
         task_local_storage()[HANDLERS_KEY] = current_handlers[1:orignal_size] 
     end
 end
-
 
 
 function to_escape(f)
@@ -100,19 +109,64 @@ function to_escape(f)
 end
 
 
+function with_restart(f, restarts)
+    return to_escape() do exit
+        #preparar os restarts 
+        current_restarts = get!(task_local_storage(), RESTARTS_KEY, Pair{Symbol, Function}[])
+        orignal_size = length(current_restarts)
+
+        for (restart, handler) in restarts
+            push!(current_restarts, (restart => (args...) -> exit(handler(args...))))
+        end
+        try
+            f()
+        finally
+            task_local_storage()[RESTARTS_KEY] = current_restarts[1:orignal_size] 
+        end
+    end
+    
+end
 
 
-function with_restart()
+function available_restart(name)
+    restarts = get(task_local_storage(), RESTARTS_KEY, Pair{Symbol, Function}[])
+    for (restart, handler) in restarts
+        if restart == name
+            return true
+        end
+    end
+    return false
+end
+
+
+function invoke_restart(name)
+    restarts = get(task_local_storage(), RESTARTS_KEY, Pair{Symbol, Function}[])
+    for (restart, handler) in restarts
+        if restart == name
+            println("Invoking restart: ", name)
+            return handler()
+            
+        end
+    end
+    println("Restart not found")
+    return false #nao encontrou nenhum restart com esse nome
+end
+
+function invoke_restart(name, args...)
+    restarts = get(task_local_storage(), RESTARTS_KEY, Pair{Symbol, Function}[])
+    for (restart, handler) in Iterators.reverse(restarts)
+        if restart == name
+            println("Invoking restart: ", name)
+            return handler(args...)
+            
+        end
+    end
+    println("Restart not found")
+    return false #nao encontrou nenhum restart com esse nome
 end
 
 
 
-function invoke_restart()
-end
-
-function with_restart(f , restarts)
-   
-end
 
 
 
@@ -125,7 +179,6 @@ end
 function reciprocal(x)
     if x == 0
         signal(DivisionByZero)  # Use `throw` instead of `error`
-        return 2
     else
         x = 1 / x
         return x +1
@@ -150,6 +203,10 @@ handling([DivisionByZero => (c) -> println("I saw a division by zero ola")]) do
             end
             reciprocal(0)
         end
+        handling([DivisionByZero => (c) -> (println("i saw a div 320"))]) do
+            reciprocal(0)
+        end
+
         reciprocal(0)
     end
     reciprocal(0)
@@ -193,7 +250,7 @@ function print_line(str, line_end=20)
             print(c)
             col += 1
             if col == line_end
-                error(EndOfLine)
+                signal(EndOfLine)
                 col = 0
             end
         end
@@ -210,3 +267,44 @@ end
 
 # print the task task_local_storage
 println(task_local_storage())
+
+
+function reciprocal(value)
+    return x =with_restart([
+        :return_zero => ()->0,
+        :return_value => identity,
+        :retry_using => reciprocal]
+        ) do
+            value == 0 ?
+            error(DivisionByZero) :
+            1/value
+    end
+end
+
+
+
+handling([DivisionByZero => c -> invoke_restart(:return_zero)]) do
+    x = reciprocal(0)
+    println("reciprocal(0) = ", x)
+end
+
+
+handling([DivisionByZero => c-> for restart in (:return_one, :return_zero, :die_horribly)
+if available_restart(restart)
+invoke_restart(restart)
+end
+end]) do
+x = reciprocal(0)
+println("reciprocal(0) = ", x)
+end 
+
+
+function infinity() 
+    return with_restart([:just_do_it => ()->1/0]) do
+        reciprocal(0)
+    end
+end
+
+handling([DivisionByZero => (c)->invoke_restart(:just_do_it)]) do
+    infinity()
+end
